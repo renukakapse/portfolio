5b3851f24ee5fc4dfe6c269b6949428d6bee7595
Renuka Kapse <renuka@gmail.com>
2026-08-27 19:09:42 +0530

    Fix build and merge conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {
-      extend: {
-      colors: {
-        primary: "#f97316", // Orange
-        secondary: "#0f172a", // Dark Blue
-      },
-    },
-    },
-  },
-  plugins: [],
-}
-
-=======
-  darkMode:"class",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      extend: {
-        colors: {
-          primary: "#f97316", // Orange
-          secondary: "#0f172a", // Dark Blue
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
->>>>>>> a8b75d03ab7250ef76a89e32c6b1bf1a9e1f6c96
